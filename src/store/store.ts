9dc2ba7e12fdaad5234ab918b41ba187152663c8
hyunwoo <khj930410@naver.com>
2024-03-03 23:29:18 +0900

    etc: 불필요한 주석 삭제

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,14 +4,6 @@ export const useStore = create<StoreState>((set) => ({
   toDoList: [],
   addItem: (item: ToDoItem) => set((state: StoreState) => ({ toDoList: [...state.toDoList, item] })),
   updateItem: (toDoItems: ToDoItem[]) => set(() => ({ toDoList: toDoItems })),
-
-  //   const newToDoList =
-
-  //   toDoList: state.toDoList.filter((_, i) => i !== index) })),
-  // bears: 0,
-  // increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  // removeAllBears: () => set({ bears: 0 }),
-  // updateBears: (newBears) => set({ bears: newBears }),
 }))
 
 export type StoreState = {
